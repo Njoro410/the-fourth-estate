0b70579bbd5889d2e1def91a13d04a8998213a40
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-26 17:56:31 +0300

    added template files

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def index():
-    return '<h1>welcome to news app</h1>'
+    return render_template('index.html')
 
 @views.route('/sources')
 def sources():
