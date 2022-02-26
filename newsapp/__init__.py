23f02cf4f8cd947d18e4766281dc50158a514a35
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-26 16:24:06 +0300

    imported blueprint for views file

--- newsapp/__init__.py
+++ newsapp/__init__.py
@@ -11,7 +11,7 @@ def create_app(config_name):
     
     
     # setting config
-    from .request import configure_request
-    configure_request(app)
+    # from .request import configure_request
+    # configure_request(app)
 
     return app
