977141a483240a9e943ce9314438700dc76cd7f0
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-26 19:04:36 +0300

    added a carousel in index page

--- newsapp/views.py
+++ newsapp/views.py
@@ -8,8 +8,8 @@ def index():
 
 @views.route('/sources')
 def sources():
-    return '<h1>list pf news sources</h1>'
+    return render_template('sources.html')
 
 @views.route('/top_headlines')
 def top_headlines():
-    return '<h1>top headlines around the world</h1>'
+    return render_template('top-headlines.html')
