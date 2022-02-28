edb834f82a4aa83fb53e752d3421c4fe973f0fa6
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-28 05:16:19 +0300

    added top headlines method to main route

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import datetime
 from flask import Blueprint,render_template,request
-from .request import fetch_sources, get_articles
+from .request import fetch_sources, get_articles, get_headlines
+
 
 
 views = Blueprint('views',__name__)
@@ -9,8 +11,9 @@ views = Blueprint('views',__name__)
 def index():
     news_sources = fetch_sources('sources')
     print(news_sources)
+    top_headlines = get_headlines('us')
 
-    return render_template('index.html',sources=news_sources)
+    return render_template('index.html',sources=news_sources,headlines=top_headlines)
 
 
 @views.route('/articles/<string:id>')
@@ -18,5 +21,9 @@ def articles(id):
 
     sources_articles = get_articles(id)
     print(sources_articles)
+    date_now = datetime.datetime.now()
+    
 
-    return render_template('articles.html',articles=sources_articles)
+    return render_template('articles.html',articles=sources_articles,date= date_now)
+
+
