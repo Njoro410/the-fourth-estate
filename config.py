1999dce3bdc770148e9289ac0e4d4343a27799c0
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-28 05:15:34 +0300

    added a url to fetch trending headlines

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/{}?apiKey=b17b95e4d3824e93a3a1a45b35bcef35'
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey=b17b95e4d3824e93a3a1a45b35bcef35'
+    TOP_URL = 'https://newsapi.org/v2/top-headlines?country={}&apiKey=b17b95e4d3824e93a3a1a45b35bcef35'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
   
