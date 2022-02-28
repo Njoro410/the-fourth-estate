fa330339f8ccba87e9a823d219e545c1f2b0c31f
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-28 05:17:21 +0300

    added a top headlines class

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class Sources:
 
 class Articles:
     """
-    Top headlines class to define articles objects
+    Articles class to define articles objects
     """
 
     def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
@@ -29,3 +29,17 @@ class Articles:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
         self.content = content
+    
+class Top:
+
+        def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
+            self.id = id
+            self.name = name
+            self.author = author
+            self.title = title
+            self.description = description
+            self.url = url
+            self.urlToImage = urlToImage
+            self.publishedAt = publishedAt
+            self.content = content
+
