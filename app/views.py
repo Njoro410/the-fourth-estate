bf66ee51b991b90703a6702c8c8db7b163cdd236
njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-28 16:12:36 +0300

    added a search method

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import datetime
-from flask import Blueprint,render_template,request
-from .request import fetch_sources, get_articles, get_headlines
+from flask import Blueprint, redirect,render_template,request,url_for
+from .request import fetch_sources, get_articles, get_headlines, search_article
 
 
 
@@ -12,8 +12,11 @@ def index():
     news_sources = fetch_sources('sources')
     print(news_sources)
     top_headlines = get_headlines('us')
-
-    return render_template('index.html',sources=news_sources,headlines=top_headlines)
+    search_article = request.args.get('query')
+    if search_article:
+        return redirect(url_for('.search', query = search_article))
+    else:
+        return render_template('index.html',isIndex=True,sources=news_sources,headlines=top_headlines)
 
 
 @views.route('/articles/<string:id>')
@@ -27,3 +30,12 @@ def articles(id):
     return render_template('articles.html',articles=sources_articles,date= date_now)
 
 
+@views.route('/search/<query>')
+def search(query):
+    '''
+    View function to display the search results
+    '''
+    article_list = query.split(" ")
+    article_list_format = "+".join(article_list)
+    searched_articles = search_article(article_list_format)
+    return render_template('search.html',s_articles=searched_articles)
